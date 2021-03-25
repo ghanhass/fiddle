65e402b8d7583bb7490ebabf3ce3519b73d91346
hgh <hassen.ghanmi@vneuron.tn>
2021-03-25 02:21:19 +0100

    updates

--- src/app/ressources/ressources.component.ts
+++ src/app/ressources/ressources.component.ts
@@ -237,30 +237,23 @@ export class RessourcesComponent implements OnInit {
   ressourceChoiceSelectedAssetDragstart(event:DragEvent){
     console.log("ressourceChoiceSelectedAssetDragstart event = ", event.target);
     let evTarget: HTMLElement = event.target as HTMLElement;
-    if(evTarget.classList.contains('ressources-choice-selected-file')){
-      this.assetIndexDragstart = parseInt(evTarget.dataset.index);
-    }
+    this.assetIndexDragstart = parseInt(evTarget.dataset.index);
   }
 
   ressourceChoiceSelectedAssetDragenter(event:DragEvent){
     console.log("ressourceChoiceSelectedAssetDragenter target = ", event.target);
     let evTarget: HTMLElement = event.target as HTMLElement;
     //evTarget.classList.add("placeholder");
-    if(evTarget.classList.contains('ressources-choice-selected-file')){
-      this.assetIndexDragstart = parseInt(evTarget.dataset.index);
-      let index = parseInt(evTarget.dataset.index);
-      this.selectedRessourceAssets[index].placeholderMode = true;
-    }
+    let index = parseInt(evTarget.dataset.index);
+    this.selectedRessourceAssets[index].placeholderMode = true;
   }
 
   ressourceChoiceSelectedAssetDragleave(event:DragEvent){
     console.log("ressourceChoiceSelectedAssetDragleave target = ", event.target);
     let evTarget: HTMLElement = event.target as HTMLElement;
     //evTarget.classList.remove("placeholder");
-    if(evTarget.classList.contains('ressources-choice-selected-file')){
-      let index = parseInt(evTarget.dataset.index);
-      this.selectedRessourceAssets[index].placeholderMode = false;
-    }
+    let index = parseInt(evTarget.dataset.index);
+    this.selectedRessourceAssets[index].placeholderMode = false;
   }
 
   getFullAssetUrl(selectedRessourceAsset: SelectedRessourceAsset){
